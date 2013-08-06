04661a1c036bd62cc08b23a7f2259a5e051698d7
Mouhcine <mouhcine.elamine@icloud.com>
2013-08-06 20:01:13 +0200

    Initial commit

---
+++ EAMTextView/EAMTextView.h
@@ -0,0 +1,113 @@
+//
+//  EAMTextView.h
+//
+//  Created by Mouhcine El Amine on 06/08/13.
+//
+//  Copyright (c) 2013 El Amine Mouhcine.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of
+//  this software and associated documentation files (the "Software"), to deal in
+//  the Software without restriction, including without limitation the rights to
+//  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+//  the Software, and to permit persons to whom the Software is furnished to do so,
+//  subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#import <UIKit/UIKit.h>
+
+@class EAMTextView;
+
+/**
+ *	The EAMTextViewDelegate protocol defines optional methods added to the set of methods defined in UITextViewDelegate.
+ */
+@protocol EAMTextViewDelegate <NSObject, UITextViewDelegate>
+
+@optional
+
+/**
+ *	Tells the delegate that the text view's height is about to change.
+ *
+ *	@param	textView	The text view whose height is about to change.
+ *	@param	oldHeight	The actual height of the text view's frame.
+ *	@param	newHeight	The new height of the text view's frame.
+ *
+ *  @discussion This method gets called in the animation block.
+ */
+- (void)textView:(EAMTextView *)textView willChangeFromHeight:(CGFloat)oldHeight toHeight:(CGFloat)newHeight;
+
+/**
+ *	Tells the delegate that the text view's height did change.
+ *
+ *	@param	textView	The text view whose height did change.
+ *	@param	oldHeight	The actual height of the text view's frame.
+ *	@param	newHeight	The new height of the text view's frame.
+ *
+ *  @discussion This method gets called in the animation completion block.
+ */
+- (void)textView:(EAMTextView *)textView didChangeFromHeight:(CGFloat)oldHeight toHeight:(CGFloat)newHeight;
+
+@end
+
+/**
+ *	EAMTextView is UITextView subclass that adds placeholder and vertical autoresizing support.
+ */
+@interface EAMTextView : UITextView
+
+/**
+ *	The object that acts as the delegate of the text view.
+ */
+@property (nonatomic, weak) id <EAMTextViewDelegate> delegate;
+
+/**
+ *	The string that is displayed when there is no other text in the text field.
+ *  
+ *  @discussion The default value is nil.
+ */
+@property (nonatomic, strong) NSString *placeholder;
+
+/**
+ *	The color of the placeholder.
+ *
+ *  @discussion The default value is [UIColor lightGrayColor].
+ */
+@property (nonatomic, strong) UIColor *placeholderColor;
+
+/**
+ *	A boolean value indicating whether the text view autoresizes vertically.
+ *
+ *  @discussion The default value is NO.
+ */
+@property (nonatomic) BOOL autoresizesVertically;
+
+/**
+ *	The minimum height for the text view.
+ *
+ *  The default value is 0.
+ */
+@property (nonatomic) CGFloat minimumHeight;
+
+/**
+ *	The maximum height for the text view.
+ *
+ *  The default value is CGFloat_MAX.
+ */
+@property (nonatomic) CGFloat maximumHeight;
+
+/**
+ *	The autoresizing animation duration.
+ *
+ *  The default value is 0.2 .
+ */
+@property (nonatomic) NSTimeInterval autoresizingAnimationDuration;
+
+@end
---
+++ EAMTextView/EAMTextView.m
@@ -0,0 +1,197 @@
+//
+//  EAMTextView.m
+//
+//  Created by Mouhcine El Amine on 06/08/13.
+//  Copyright (c) 2013 El Amine Mouhcine.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of
+//  this software and associated documentation files (the "Software"), to deal in
+//  the Software without restriction, including without limitation the rights to
+//  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+//  the Software, and to permit persons to whom the Software is furnished to do so,
+//  subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#import "EAMTextView.h"
+
+static CGFloat const EAMTextViewMinimumHeight = 0.0f;
+static CGFloat const EAMTextViewMaximumHeight = CGFLOAT_MAX;
+static NSTimeInterval const EAMTextViewAutoresizingAnimationDuration = 0.2f;
+static CGFloat const EAMTextViewPlaceholderInset = 8.0f;
+
+@implementation EAMTextView
+
+@synthesize placeholderColor = _placeholderColor;
+
+#pragma mark -
+#pragma mark Initialization
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self _baseInit];
+    }
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self _baseInit];
+    }
+    return self;
+}
+
+- (void)_baseInit
+{
+    _autoresizesVertically = NO;
+    _minimumHeight = EAMTextViewMinimumHeight;
+    _maximumHeight = EAMTextViewMaximumHeight;
+    _autoresizingAnimationDuration = EAMTextViewAutoresizingAnimationDuration;
+    [self _startObservingTextChangeNotification];
+}
+
+- (void)dealloc
+{
+    [self _stopObservingTextChangeNotification];
+}
+
+- (void)_startObservingTextChangeNotification
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(_textChanged)
+                                                 name:UITextViewTextDidChangeNotification
+                                               object:self];
+}
+
+- (void)_stopObservingTextChangeNotification
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UITextViewTextDidChangeNotification
+                                                  object:self];
+}
+
+#pragma mark -
+#pragma mark Placeholder
+
+- (void)setPlaceholder:(NSString *)placeholder
+{
+    if (![_placeholder isEqualToString:placeholder]) {
+        _placeholder = placeholder;
+        if ([self _shouldDrawPlaceholder]) {
+            [self setNeedsDisplay];
+        }
+    }
+}
+
+- (UIColor *)placeholderColor
+{
+    if (!_placeholderColor) {
+        _placeholderColor = [UIColor lightGrayColor];
+    }
+    return _placeholderColor;
+}
+
+- (void)setPlaceholderColor:(UIColor *)placeholderColor
+{
+    _placeholderColor = placeholderColor;
+    if ([self _shouldDrawPlaceholder]) {
+        [self setNeedsDisplay];
+    }
+}
+
+- (CGRect)_placeholderRectInRect:(CGRect)rect
+{
+    return CGRectInset(rect, EAMTextViewPlaceholderInset, EAMTextViewPlaceholderInset);
+}
+
+- (BOOL)_shouldDrawPlaceholder
+{
+    return ![self hasText] && self.placeholder.length > 0;
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    [super drawRect:rect];
+    if ([self _shouldDrawPlaceholder]) {
+        [self.placeholderColor set];
+        [self.placeholder drawInRect:[self _placeholderRectInRect:rect]
+                            withFont:self.font
+                       lineBreakMode:NSLineBreakByTruncatingTail
+                           alignment:self.textAlignment];
+    }
+}
+
+#pragma mark-
+#pragma mark Autoresizing
+
+- (void)setAutoresizesVertically:(BOOL)autoresizesVertically
+{
+    _autoresizesVertically = autoresizesVertically;
+    [self _textChanged];
+}
+
+- (void)setMaximumHeight:(CGFloat)maximumHeight
+{
+    _maximumHeight = maximumHeight;
+    [self _textChanged];
+}
+
+-(void)setMinimumHeight:(CGFloat)minimumHeight
+{
+    _minimumHeight = minimumHeight;
+    [self _textChanged];
+}
+
+- (void)_textChanged
+{
+    if (self.autoresizesVertically) {
+        // stay within min and max limit
+        CGFloat newHeight = MIN(self.contentSize.height, self.maximumHeight);
+        newHeight = MAX(newHeight, self.minimumHeight);
+        
+        CGFloat oldHeight = self.frame.size.height;
+        if (oldHeight != newHeight) {
+            
+            // Frame needs to be changed
+            CGRect newFrame = self.frame;
+            CGFloat heightDifference = newHeight - oldHeight;
+            newFrame.size.height += heightDifference;
+            
+            // Animate change
+            [UIView animateWithDuration:self.autoresizingAnimationDuration
+                                  delay:0.0f
+                                options:UIViewAnimationOptionAllowUserInteraction | UIViewAnimationOptionBeginFromCurrentState
+                             animations:^{
+                                 if ([self.delegate respondsToSelector:@selector(textView:willChangeFromHeight:toHeight:)]) {
+                                     [self.delegate textView:self
+                                        willChangeFromHeight:oldHeight
+                                                    toHeight:newHeight];
+                                 }
+                                 self.frame = newFrame;
+                             }
+                             completion:^(BOOL finished) {
+                                 if ([self.delegate respondsToSelector:@selector(textView:didChangeFromHeight:toHeight:)]) {
+                                     [self.delegate textView:self
+                                         didChangeFromHeight:oldHeight
+                                                    toHeight:newHeight];
+                                 }
+                             }];
+        }
+    }
+    [self setNeedsDisplay];
+}
+
+@end
